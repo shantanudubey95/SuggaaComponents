a689410831a87a5587a93b72834a049346acf3ea
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-08-18 12:59:01 +0530

    Installed reanimated for animated text

---
+++ src/AnimatedText.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import Animated, {
+  useDerivedValue,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  Easing
+} from "react-native-reanimated";
+import { ReText } from "react-native-redash";
+
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+
+const COUNT_DOWN_FROM = 30;
+
+const AnimatedText = () => {
+  const count = useSharedValue(COUNT_DOWN_FROM);
+  const formatted = useDerivedValue(() => (`${Math.floor(count.value)}`.padStart(2, '0')));
+
+  useEffect(() => {
+    count.value = withTiming(0, { duration: COUNT_DOWN_FROM * 1000, easing: Easing.linear }, () => {
+      console.log("Finished with timer")
+    });
+  }, [count])
+
+  return (
+    <ReText
+      text={formatted}
+      style={{ color: "red", fontVariant: ["tabular-nums"], fontSize: 50 }}
+    />
+  )
+}
+
+
+
+export default AnimatedText;
