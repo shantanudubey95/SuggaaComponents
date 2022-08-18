2710f903bfc5266b15ec38011a3ce408009a3080
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-08-18 13:23:52 +0530

    Date input and progress bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import OTPInputs from './src/OTPInputs';
+import AnimatedText from './src/AnimatedText';
+import ProgressAnimation from "./src/ProgressAnimation";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <OTPInputs numberOfInputs={4}/>
+      <AnimatedText />
+      <ProgressAnimation
+      activeColor="#04825C"
+      trackColor="#04825C40"
+      progress={100}
+      steps={[
+        { status: "done", label: "Personal Details" },
+        { status: "untouched", label: "Bank Details" },
+        { status: "active", label: "Vehicle Details" }
+      ]}
+
+    />
       <StatusBar style="auto" />
     </View>
   );
