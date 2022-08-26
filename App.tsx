e7b624cffd05f43747db732fa5106a1963e37e5a
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-08-26 12:20:15 +0530

    Updated image import & added image to asset folder

--- App.tsx
+++ App.tsx
@@ -1,14 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import OTPInputs from './src/OTPInputs';
-import AnimatedText from './src/AnimatedText';
-import ProgressAnimation from "./src/ProgressAnimation";
-import DateInput from './src/MaskedText/DateInput';
-import SuggaaTextInput from './src/SuggaaTextInput';
-
-import tw from 'twrnc'
-import ReferCode from './src/ReferCode';
-import AppLoading from 'expo-app-loading';
 import {
   useFonts,
   Poppins_300Light,
@@ -18,14 +7,25 @@ import {
   Poppins_700Bold,
   Poppins_600SemiBold,
   Poppins_900Black,
-} from "@expo-google-fonts/poppins";
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import PressableButton from './src/PressableButton';
+import { Button, StyleSheet, View } from 'react-native';
+import tw from 'twrnc';
+
+import AnimatedText from './src/AnimatedText';
+import DateInput from './src/MaskedText/DateInput';
+import OTPInputs from './src/OTPInputs';
 import OnOffDutySwitch from './src/OnOffDutySwitch';
+import PressableButton from './src/PressableButton';
+import ProgressAnimation from './src/ProgressAnimation';
+import ReferCode from './src/ReferCode';
 import SuggaaSelectButton from './src/SuggaaSelectButton';
+import SuggaaTextInput from './src/SuggaaTextInput';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Poppins_300Light,
     Poppins_400Regular,
     Poppins_600SemiBold,
@@ -42,69 +42,75 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-  return (
-    <View style={styles.container}>
-      <OTPInputs numberOfInputs={4} />
-      <AnimatedText />
-      <ProgressAnimation
-        activeColor="#04825C"
-        trackColor="#04825C40"
-        progress={100}
-        steps={[
-          { status: "done", label: "Personal Details" },
-          { status: "untouched", label: "Bank Details" },
-          { status: "active", label: "Vehicle Details" }
-        ]}
-      />
+    return (
+      <View style={styles.container}>
+        <OTPInputs numberOfInputs={4} />
+        <AnimatedText />
+        <ProgressAnimation
+          activeColor="#04825C"
+          trackColor="#04825C40"
+          progress={100}
+          steps={[
+            { status: 'done', label: 'Personal Details' },
+            { status: 'untouched', label: 'Bank Details' },
+            { status: 'active', label: 'Vehicle Details' },
+          ]}
+        />
 
-      <DateInput
-        color={"text-green-600"}
-        textStyle={[tw`font-bold text-green-600 text-lg text-center tabular-nums`]}
-        viewStyle={[tw`flex-row py-4 px-8 rounded-2 items-center border-2 border-green-600`]} />
+        <DateInput
+          color="text-green-600"
+          textStyle={[tw`font-bold text-green-600 text-lg text-center tabular-nums`]}
+          viewStyle={[tw`flex-row py-4 px-8 rounded-2 items-center border-2 border-green-600`]}
+        />
 
-      <ReferCode code={"SUGGAREF25"} />
+        <ReferCode code="SUGGAREF25" />
 
-      <View style={{ width: '100%', paddingHorizontal: 54 }}>
-                <SuggaaTextInput
-                    style={{
-                        alignContent: 'center',
-                        width: '100%',
-                        borderWidth: 2,
-                        borderColor: '#04825C',
-                        borderRadius: 5,
-                        fontSize: 20,
-                        fontWeight: '400',
-                        paddingHorizontal: 13,
-                        paddingVertical: 10,
-                    }}
-                    value={value}
-                    label="Full name"
-                    errorText={error}
-                    onChangeText={(text) => setValue(text)}
-                    selectionColor="#04825C"
-                />
-            </View>
-            <View style={{ height: 16 }} />
-            <Button title={error ? "Clear error" : "Set Error"} onPress={() => error ? setError("") : setError('This field is required.')} />
-            <PressableButton onPress={() => {}} icon={SHARE} />
-            <OnOffDutySwitch />
-            <SuggaaSelectButton  title="Him"
+        <View style={{ width: '100%', paddingHorizontal: 54 }}>
+          <SuggaaTextInput
+            style={{
+              alignContent: 'center',
+              width: '100%',
+              borderWidth: 2,
+              borderColor: '#04825C',
+              borderRadius: 5,
+              fontSize: 20,
+              fontWeight: '400',
+              paddingHorizontal: 13,
+              paddingVertical: 10,
+            }}
+            value={value}
+            label="Full name"
+            errorText={error}
+            onChangeText={(text) => setValue(text)}
+            selectionColor="#04825C"
+          />
+        </View>
+        <View style={{ height: 16 }} />
+        <Button
+          title={error ? 'Clear error' : 'Set Error'}
+          onPress={() => (error ? setError('') : setError('This field is required.'))}
+        />
+        <PressableButton onPress={() => {}} icon={SHARE} />
+        <OnOffDutySwitch />
+        <SuggaaSelectButton
+          title="Him"
           onPress={(value) => setSelectedCheckBox(value)}
-          isActive={selectedCheckBox === 'Him'}/>
-          <SuggaaSelectButton
+          isActive={selectedCheckBox === 'Him'}
+        />
+        <SuggaaSelectButton
           title="Her"
           onPress={(value) => setSelectedCheckBox(value)}
           isActive={selectedCheckBox === 'Her'}
         />
-          <SuggaaSelectButton
+        <SuggaaSelectButton
           title="I perfer not to say"
           onPress={(value) => setSelectedCheckBox(value)}
           isActive={selectedCheckBox === 'I perfer not to say'}
         />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/AnimatedText.tsx
+++ src/AnimatedText.tsx
@@ -1,35 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
 import Animated, {
   useDerivedValue,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
-  Easing
-} from "react-native-reanimated";
-import { ReText } from "react-native-redash";
-
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+  Easing,
+} from 'react-native-reanimated';
+import { ReText } from 'react-native-redash';
 
 const COUNT_DOWN_FROM = 30;
 
 const AnimatedText = () => {
   const count = useSharedValue(COUNT_DOWN_FROM);
-  const formatted = useDerivedValue(() => (`${Math.floor(count.value)}`.padStart(2, '0')));
+  const formatted = useDerivedValue(() => `${Math.floor(count.value)}`.padStart(2, '0'));
 
   useEffect(() => {
-    count.value = withTiming(0, { duration: COUNT_DOWN_FROM * 1000, easing: Easing.linear }, () => {
-    });
-  }, [count])
+    count.value = withTiming(
+      0,
+      { duration: COUNT_DOWN_FROM * 1000, easing: Easing.linear },
+      () => {}
+    );
+  }, [count]);
 
   return (
     <ReText
       text={formatted}
-      style={{ color: "red", fontVariant: ["tabular-nums"], fontSize: 50 }}
+      style={{ color: 'red', fontVariant: ['tabular-nums'], fontSize: 50 }}
     />
-  )
-}
-
-
+  );
+};
 
 export default AnimatedText;
