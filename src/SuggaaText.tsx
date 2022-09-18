b514c141c48450977ac20e60af00ae9255239ae1
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-09-18 19:58:05 +0530

    eslint fix

--- src/SuggaaText.tsx
+++ src/SuggaaText.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Text, TextProps } from 'react-native'
+import React from 'react';
+import { Text, TextProps } from 'react-native';
 
 type Props = TextProps & {
-    type: 'bold' | 'semibold' | 'medium' | 'regular';
-    text: string;
-}
+  type: 'bold' | 'semibold' | 'medium' | 'regular';
+  text: string;
+};
 
 export default function SuggaaText(props: Props) {
-    return (
-        <Text {...props} style={[{
-            fontFamily: props.type === 'bold' ? 'Poppins_700Bold'
-                : props.type === 'semibold' ? 'Poppins_600SemiBold'
-                    : props.type === 'medium' ? 'Poppins_500Medium'
-                        : props.type === 'regular' ? 'Poppins_400Regular'
-                            : ''
-        }, props.style]} >
-
-            {props.text}
-
-        </Text>
-    )
+  return (
+    <Text
+      {...props}
+      style={[
+        {
+          fontFamily:
+            props.type === 'bold'
+              ? 'Poppins_700Bold'
+              : props.type === 'semibold'
+              ? 'Poppins_600SemiBold'
+              : props.type === 'medium'
+              ? 'Poppins_500Medium'
+              : props.type === 'regular'
+              ? 'Poppins_400Regular'
+              : '',
+        },
+        props.style,
+      ]}>
+      {props.text}
+    </Text>
+  );
 }
